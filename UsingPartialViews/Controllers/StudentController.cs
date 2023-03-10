d52378b4b60144807540ab6bfbe88f2810536322
Samiksha Fulambarkar <samikshafulambarkar99@gmail.com>
2023-03-10 13:52:10 +0530

    Add project files.

---
+++ UsingPartialViews/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsingPartialViews.Data;
+
+namespace UsingPartialViews.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public StudentController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var model = applicationDbContext.Students.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ UsingPartialViews/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsingPartialViews.Models
+{
+    [Table("student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? City { get; set; }
+    }
+}
